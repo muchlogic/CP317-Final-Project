e03cfb4e48645a07bea1e9d4d8b911644b5ae500
gluckymcdoodle <palu7714@mylaurier.ca>
2024-11-27 19:32:41 -0500

    login page

---
+++ src/routes/UserManagement/EditProfileInterface.jsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+import { useState, useEffect } from "react";
+
+export default function EditProfileInterface() {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+}
---
+++ src/routes/UserManagement/SignUpInterface.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+import { useState, useEffect } from "react";
+
+export default function SingUpInterface() {
+  return (
+    <>
+      <Container className="flex justify-center items-center h-[100vh]">
+      </Container>
+    </>
+  );
+}
