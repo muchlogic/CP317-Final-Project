4eb33de923dc787d7c488bafb271fe853cf8fe30
gluckymcdoodle <palu7714@mylaurier.ca>
2024-11-29 19:13:12 -0500

    signup mostly done

--- Frontend/src/routes/UserManagement/LoginInterface.jsx
+++ Frontend/src/routes/UserManagement/LoginInterface.jsx
@@ -7,13 +7,13 @@ import {alignProperty} from "@mui/material/styles/cssUtils";
 export default function LoginInterface() {
 const [username, setUsername] = useState("")
 const [password, setPassword] = useState("")
-const [error, setError] = useState("")
+const [error, displayError] = useState("")
 
-const handleLogin = async (event) => {
+const loginUser = async (event) => {
   event.preventDefault();
 
   try {
-    const response = await fetch("/users/login", {
+    const response = await fetch("http:localhost:3000/users/login", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -25,29 +25,29 @@ const handleLogin = async (event) => {
     if (response.ok) {
       // Successful login
       console.log("Success");
-      setError("");
+      displayError("");
     } else {
       const errorReason = await response.json();
       if (errorReason.message === "Invalid Password") {
-        setError("Password does not match associated email.");
+        displayError("Password does not match associated email.");
       } else if (errorReason.message === "Invalid Username") {
-        setError("Must be a username that exists in the database.");
+        displayError("Must be a username that exists in the database.");
       } else {
-        setError("Unknown Error");
+        displayError("Invalid");
       }
     }
   } catch (error) {
     console.error("Error during login:", error);
-    setError("Unknown Error");
+    displayError("Invalid username or password");
   }
 };
   return (
 
     <>
   <Container className="flex justify-center items-center h-[100vh]">
-      <form onSubmit={handleLogin} className="space-y-4">
+      <form onSubmit={loginUser} className="space-y-4">
         <h1 className="text-3xl font-bold"style={{marginBottom:"25px"}}>Login</h1>
-       
+       {error && <p className="text-red-500">{error}</p>}
 
         <div className="input-box space-y-4">
         <TextField 
